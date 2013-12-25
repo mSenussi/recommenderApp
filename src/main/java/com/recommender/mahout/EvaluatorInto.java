59d1eb60ae86bb30d7200a01a7cb8b8f4a82b175
MuhammedSenussi <muhammed.senussi@silicon-nile.com>
2013-12-25 17:17:57 +0200

    Implementing IRstatsEvaluation

--- src/main/java/com/recommender/mahout/EvaluatorInto.java
+++ src/main/java/com/recommender/mahout/EvaluatorInto.java
@@ -1,18 +1,25 @@
 package com.recommender.mahout;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.DataModelBuilder;
+import org.apache.mahout.cf.taste.eval.IRStatistics;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
 import org.apache.mahout.cf.taste.impl.common.FastByIDMap;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.IDRescorer;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 import org.apache.mahout.common.RandomUtils;
@@ -24,10 +31,77 @@ public class EvaluatorInto {
 	private static final Logger LOG = LoggerFactory
 			.getLogger(EvaluatorInto.class);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	
+	
+	
+	public static double evalAvarage(DataModel model, double trainingPercentage, double evalPercentage) throws TasteException {
+		
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		
+		RecommenderBuilder recommenderBuilder = new  RecommenderBuilder() {
+			
+			@Override
+			public Recommender buildRecommender(DataModel dataModel)
+					throws TasteException {
+
+				UserSimilarity userSimilarity = new PearsonCorrelationSimilarity(dataModel);
+				UserNeighborhood userNeighborhood = new NearestNUserNeighborhood(2, userSimilarity, dataModel);
+				
+				
+				return new GenericUserBasedRecommender(dataModel, userNeighborhood, userSimilarity);
+			}
+		};
+		long start = System.currentTimeMillis();
+		double score = evaluator.evaluate(recommenderBuilder, null, model, trainingPercentage, evalPercentage);
+		long stop = System.currentTimeMillis();
+		LOG.info(" Took: [" + (stop - start) + " millis | "+(stop - start)/1000+" Sec ]");
+		return score;
+	}
+	
+    public static double evalRMS(DataModel model, double trainingPercentage, double evalPercentage) throws TasteException{
+    	
+    	RecommenderEvaluator evaluator = new RMSRecommenderEvaluator();
+    	RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+			
+			@Override
+			public Recommender buildRecommender(DataModel dataModel)
+					throws TasteException {
+				UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+				UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, dataModel);
+				
+				return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+			}
+		};
+    	
+		long start = System.currentTimeMillis();
+		double score = evaluator.evaluate(recommenderBuilder, null, model, trainingPercentage, evalPercentage);
+		long stop = System.currentTimeMillis();
+		LOG.info(" Took: [" + (stop - start) + " millis | "+(stop - start)/1000+" Sec ]");
+		return score;
+	}
+	
+	
+    public static IRStatistics evalIRStats(DataModel model) throws TasteException {
+    	
+    	RecommenderIRStatsEvaluator evaluator = new GenericRecommenderIRStatsEvaluator();
+    	RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+			
+			@Override
+			public Recommender buildRecommender(DataModel dataModel)
+					throws TasteException {
+				UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+				UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, dataModel);
+				return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+			}
+		};
+		long start = System.currentTimeMillis();
+		IRStatistics irStatistics = evaluator.evaluate(recommenderBuilder, null, model, null, 2, GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 0.2);
+		long stop = System.currentTimeMillis();
+		LOG.info(" Took: [" + (stop - start) + " millis | "+(stop - start)/1000+" Sec ]");
+		return irStatistics;
+	}
+    
+    public static void main(String[] args) {
 
 		File userPreferencesFile;
 		try {
@@ -36,29 +110,14 @@ public class EvaluatorInto {
 					"src/main/resources/data/dataset.txt");
 
 			DataModel model =  new FileDataModel(userPreferencesFile);
-			RecommenderEvaluator evaluator =
-				      new AverageAbsoluteDifferenceRecommenderEvaluator();
 			
-			RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
-				@Override
-				public Recommender buildRecommender(DataModel model)
-						throws TasteException {
-					
-					UserSimilarity similarity = new PearsonCorrelationSimilarity (model);
-					 UserNeighborhood neighborhood =
-					 new NearestNUserNeighborhood (2, similarity, model);
-					 
-					 return  new GenericUserBasedRecommender (model, neighborhood, similarity);
-					
-				}
-			};
-						
-			long start = System.currentTimeMillis();
-			// Use 70% of the data to train; test using the other 30%.
-		    double score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 0.2);
-		    long stop = System.currentTimeMillis();
-		    System.out.println(score);
-		    LOG.info(" Took: [" + (stop - start) + " millis | "+(stop - start)/1000+" Sec ]");
+//			double avarEvaScore = evalAvarage(model, 0.8, 0.3);
+//			double rMSEvaScore = evalRMS(model, 0.8, 0.3);
+//			System.out.println("AverageAbsoluteDifference: " + new DecimalFormat("##.##").format(avarEvaScore));
+//		    System.out.println("RMSDifference: " + new DecimalFormat("##.##").format(rMSEvaScore));
+		    
+			IRStatistics iRStats = evalIRStats(model);
+			System.out.println(new DecimalFormat("##.##").format(iRStats.getRecall()));
 
 		} catch (Exception e) {
 			LOG.error(e.getMessage() + "\n--------\n" + e.getStackTrace());
