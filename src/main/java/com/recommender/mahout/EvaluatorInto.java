7295366db7c3e0e5968772dca67558e5d1659a0e
MuhammedSenussi <muhammed.senussi@silicon-nile.com>
2013-12-30 15:32:46 +0200

    using grouplense M

--- src/main/java/com/recommender/mahout/EvaluatorInto.java
+++ src/main/java/com/recommender/mahout/EvaluatorInto.java
@@ -7,12 +7,15 @@ import org.apache.mahout.cf.taste.eval.IRStatistics;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.example.grouplens.GroupLensDataModel;
+import org.apache.mahout.cf.taste.example.grouplens.GroupLensRecommender;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
 import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.slopeone.SlopeOneRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
@@ -95,33 +98,36 @@ public class EvaluatorInto {
 		LOG.info(" Took: [" + (stop - start) + " millis | "+(stop - start)/1000+" Sec ]");
 		return irStatistics;
 	}
-    
+        
     public static void main(String[] args) {
 
-		File userPreferencesFile, testFile, groupLensFile;
+		File userPreferencesFile, testFile, groupLensFileK, groupLensFileM;
 		try {
 
 			userPreferencesFile = new File(
 					"src/main/resources/data/dataset.txt");
 			testFile = new File(
 					"src/main/resources/data/introDataset.txt");
-			groupLensFile = new File(
-					"src/main/resources/data/ml-100k/ua.base");
+			groupLensFileK = new File(
+					"src/main/resources/data/gl/ua.base");
+			groupLensFileM = new File(
+					"src/main/resources/data/gl/ratings.dat");
 
-			DataModel model     =  new FileDataModel(userPreferencesFile);
-			DataModel testModel =  new FileDataModel(testFile);
-			DataModel gLModel   =  new FileDataModel(groupLensFile);
+			DataModel model     = new FileDataModel(userPreferencesFile);
+			DataModel testModel = new FileDataModel(testFile);
+			DataModel gLModelK  = new FileDataModel(groupLensFileK);
+			DataModel gLModelM	= new GroupLensDataModel(groupLensFileM);
 			
 //			double avarEvaScore = evalAvarage(model, 0.8, 0.3);
 //			double rMSEvaScore = evalRMS(model, 0.8, 0.3);
 //			System.out.println("AverageAbsoluteDifference: " + new DecimalFormat("##.##").format(avarEvaScore));
 //		    System.out.println("RMSDifference: " + new DecimalFormat("##.##").format(rMSEvaScore));
 		    
-			IRStatistics iRStats = evalIRStats(testModel, 2, 0.2);
+			IRStatistics iRStats = evalIRStats(gLModelM, 5, 0.02);
 			
 			System.out.println(iRStats.getPrecision());
 			System.out.println(iRStats.getRecall());
-
+			
 		} catch (Exception e) {
 			LOG.error(e.getMessage() + "\n--------\n" + e.getStackTrace());
 		}
